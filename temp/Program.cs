1ab0edd343db6c2aef9abc28d37233cad5cb1d77
Christopher Smith <smith@socdefsurface.ad.socdef.com>
2020-01-13 18:33:33 -0800

    Added files from day 1 exercises and homework

---
+++ temp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace temp
+{
+    class Program
+    {
+	// Void doesnt return an value
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
