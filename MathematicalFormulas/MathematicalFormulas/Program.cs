b14ff18633127de1e71bad0826b4f76fa0c3eeb9
Chris <phatman1776@gmail.com>
2020-01-14 23:04:58 -0800

    Homework done

--- MathematicalFormulas/MathematicalFormulas/Program.cs
+++ MathematicalFormulas/MathematicalFormulas/Program.cs
@@ -7,7 +7,6 @@ namespace MathematicalFormulas
         static void Main(string[] args)
         {
             // Part 1
-            // Partially worked example
             Console.WriteLine("\nPart 1, circumference and area of a circle.");
             Console.Write("Enter an integer for the radius: ");
             string strradius = Console.ReadLine();
@@ -16,13 +15,13 @@ namespace MathematicalFormulas
             Console.WriteLine($"The circumference is {circumference}");
 
             // Implementation for area here
-            double area = Math.PI * Math.Pow(intradius,2);
+            double area = Math.PI * Math.Pow(intradius, 2);
             Console.WriteLine($"The area is {area}");
 
 
             //// Part 2
             Console.WriteLine("\nPart 2, volume of a hemisphere.");
-            Console.Write("Enter an integer for the radius of the Hemisphere");
+            Console.Write("Enter an integer for the radius of the Hemisphere: ");
             string strHemRadius = Console.ReadLine();
             int intHemRadius = int.Parse(strHemRadius);
             double volume = (2.0 / 3.0) * Math.PI * Math.Pow(intHemRadius, 3);
@@ -30,20 +29,52 @@ namespace MathematicalFormulas
 
 
             //// Part 3
-            //Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
+            Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
+            Console.WriteLine("\nEnter an integer for each of the three sides:");
+            Console.Write("Side A: ");
+            string strSideA = Console.ReadLine();
+            Console.Write("Side B: ");
+            string strSideB = Console.ReadLine();
+            Console.Write("Side C: ");
+            string strSideC = Console.ReadLine();
+            double dblSideA = double.Parse(strSideA);
+            double dblSideB = double.Parse(strSideB);
+            double dblSideC = double.Parse(strSideC);
+            //Find half of the circumference
+            double halfCirc = (dblSideA + dblSideB + dblSideB) / 2;
 
-            //// Implementation here
+            double areaTriangle = Math.Sqrt(halfCirc * (halfCirc - dblSideA) * (halfCirc - dblSideB) * (halfCirc - dblSideC));
+            Console.WriteLine($"The area is {areaTriangle}");
 
-            //Console.WriteLine($"The area is {area}");
+            // Part 4
+            Console.WriteLine("\nPart 4, solving a quadratic equation.");
+            Console.WriteLine("\nEnter an integer for the a, b,and c values: ");
+            Console.Write("Value A: ");
+            string strValueA = Console.ReadLine();
+            Console.Write("Value B: ");
+            string strValueB = Console.ReadLine();
+            Console.Write("Value C: ");
+            string strValueC = Console.ReadLine();
+            
+            //Convert strings to integers 
+            int intValueA = int.Parse(strValueA);         
+            int intValueB = int.Parse(strValueB);
+            int intValueC = int.Parse(strValueC);
 
-
-            //// Part 4
-            //Console.WriteLine("\nPart 4, solving a quadratic equation.");
-
-            //// Implementation here
-
-            //Console.WriteLine($"The positive solution is {positive_num / denominator}");
-            //Console.WriteLine($"The negative solution is {negative_num / denominator}");
+            double dblToSquare = Math.Pow(intValueB, 2.0) - (4.0 * intValueA * intValueC);
+            if (dblToSquare < 0)
+            {
+                Console.WriteLine("The values supplied are not viable solutions.");
+                return;
+            }
+            else
+            {
+                double positive_num = (-intValueB) + (Math.Sqrt(dblToSquare));
+                double negative_num = (-intValueB) - (Math.Sqrt(dblToSquare));
+                double denominator = 2.0 * intValueA;
+                Console.WriteLine($"The positive solution is {positive_num / denominator}");
+                Console.WriteLine($"The negative solution is {negative_num / denominator}");
+            }
         }
     }
 }
