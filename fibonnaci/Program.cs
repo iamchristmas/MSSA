3a2a3b8e4c39a66b82a92a987b75bdc5705bb23a
Chris <phatman1776@gmail.com>
2020-01-24 20:21:04 -0800

    added recursive fibonnaci sequence

---
+++ fibonnaci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fibonnaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fibonacciSequence(0, 1);
+        }
+        private static void fibonacciSequence(double x,double y, int count = 0)
+        {
+            if (count <= 40) {fibonacciSequence(y, x+y, ++count);}
+            else{Console.WriteLine($"the solution is {(x + y) / y}");}
+            return;
+        }
+    }
+}
