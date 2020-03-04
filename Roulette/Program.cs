4b7e8bf0dbff9e3b0f76a92e1d80198730e7f954
Chris <phatman1776@gmail.com>
2020-03-03 23:54:53 -0800

    Updated roulette

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -7,7 +7,58 @@ namespace Roulette
         static void Main(string[] args)
         {
             Roulette r = new Roulette();
-            for (int i = 0; i < 100; i++) Console.WriteLine(Roulette.Spin(r));
+            Console.Write("Please pick a bet type by the corresponding value..And I assure, We did not implement any validation: \n"
+                + "0) Bucket \n" 
+                + "1) Even or Odd \n" 
+                + "2) Colors \n" 
+                + "3) Big or small value Bucket \n" 
+                + "4) Dozens \n" 
+                + "5) Columns \n" 
+                + "6) Streets \n" 
+                + "7) 6 Numbers \n" 
+                + "8) Splits \n" 
+                + "9) Corners \n" );
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Spinning........");
+            Roulette.Spin(r);
+            BetChoice(choice);
+            Console.WriteLine($"You landed on {Roulette.b}");
+        }
+        public static void BetChoice(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    Console.WriteLine(Roulette.Getb());
+                    break;
+                case 1:
+                    Console.WriteLine($"{Roulette.EvenOdd()}");
+                    break;
+                case 2:
+                    Roulette.Colors();
+                    break;
+                case 3:
+                    Console.WriteLine(Roulette.Halves());
+                    break;
+                case 4:
+                    Console.WriteLine(Roulette.Dozens());
+                    break;
+                case 5:
+                    Roulette.Columns();
+                    break;
+                case 6:
+                    Roulette.Streets();
+                    break;
+                case 7:
+                    Roulette.SixNums();
+                    break;
+                case 8:
+                    Roulette.Splits();
+                    break;
+                case 9:
+                    Roulette.Corners();
+                    break;
+            }
         }
     }
 }
--- Roulette/Roulette.cs
+++ Roulette/Roulette.cs
@@ -4,16 +4,16 @@ using System.Text;
 
 namespace Roulette
 {
-    enum colors : short { red, black, green };
-
     class Roulette
     {
-        public short[][] RoulNums = new short[3][];
+        public static short[][] RoulNums = new short[3][];
         Random bucket = new Random();
-        public static int BucketVal = 0;
+        public static int b = 0;
         public static int NumBuckets = 38;
+        public static int col;
 
-        public Roulette() 
+
+        public Roulette()
         {
             RoulNums[0] = new short[] { 0, 100 }; // green
             RoulNums[1] = new short[] { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 }; //red
@@ -22,59 +22,102 @@ namespace Roulette
 
         public static int Spin(Roulette r)
         {
-            int col = r.bucket.Next(0, 10000) % 3;
-            int val = r.bucket.Next(0, 10000) % r.RoulNums[col].Length;
-            return BucketVal = r.RoulNums[col][val];
+            col = r.bucket.Next(0, 10000) % 3;
+            int val = r.bucket.Next(0, 10000) % RoulNums[col].Length;
+            return b = RoulNums[col][val];
         }
-        public int GetBucketVal() => Roulette.BucketVal;
+        public static int Getb() => Roulette.b;
+        public static string EvenOdd() => b % 2 == 0 ? "Even" : "Odd";
+        public static void Colors() => Console.WriteLine(string.Join(", ", Roulette.RoulNums[col]));
+        public static string Halves() => b < 19 ? "1-18" : "19-36";
 
-        public static string Halves( int BucketVal ) => BucketVal < 19 ? "1-18" : "19-36";
-        public static string EvenOdd(int BucketVal) => BucketVal % 2 == 0 ? "Even" : "Odd";
-        public static string Dozens(int BucketVal)
+        public static string Dozens()
         {
-            if (BucketVal <= 12) return "1,2,3,4,5,6,7,8,9,10,11,12";
-            else if (BucketVal > 12 && BucketVal <= 24) return "13,14,15,16,17,18,19,20,21,22,23,24";
+            if (b == 0 || b == 100) return "That's not part of a dozen";
+            else if (b <= 12) return "1,2,3,4,5,6,7,8,9,10,11,12";
+            else if (b > 12 && b <= 24) return "13,14,15,16,17,18,19,20,21,22,23,24";
             else return "25,26,27,28,29,30,31,32,33,34,35,36";
         }
-        public static void Columns(int BucketVal)
+        public static void Columns()
         {
-            int x = BucketVal % 3;
+            if (b == 0 || b == 100) Console.WriteLine("That's not part of a column");
+            int x = b % 3;
             int[] y = new int[12];
-            for (int z = 0, i = 0; i < Roulette.NumBuckets; z++, i += 3) y[z] = i;
-            Console.WriteLine(string.Join(", ", 7));
+            for (int z = 0, i = x; i < Roulette.NumBuckets; z++, i += 3) y[z] = i;
+            Console.WriteLine(string.Join(", ", y));
         }
-        public static void Streets(int BucketVal)
+        public static void Streets()
         {
-            int x = BucketVal % 3;
+            if (b == 0 || b == 100) Console.WriteLine("That's not part of a street");
+            int x = b % 3;
             string result = "";
-            if (x == 0) result = $"{BucketVal - 2},{BucketVal - 1},{BucketVal}";
-            if (x == 1) result = $"{BucketVal},{BucketVal+1},{BucketVal+2}";
-            if (x == 2) result = $"{BucketVal -1},{BucketVal },{BucketVal + 1}";
+            if (x == 0) result = $"{b - 2},{b - 1},{b}";
+            if (x == 1) result = $"{b},{b + 1},{b + 2}";
+            if (x == 2) result = $"{b - 1},{b },{b + 1}";
             Console.WriteLine(result);
         }
-        public static void Splits(int BucketVal)
+        public static void SixNums()
         {
-            int x = BucketVal % 3;
-            if ()
-            if (BucketVal < 34)
+            if (b == 0 || b == 100) Console.WriteLine("That's not part of a set of 6");
+            Streets();
+            if (b < 3)
             {
-                if (x == 0)
-                {
-                Console.WriteLine($"{BucketVal}/{BucketVal - 3}, {BucketVal}/{BucketVal + 3}," +
-                    $"{BucketVal}/{BucketVal - 1}");
-                }
-                else if (x == 1)
-                {
-                    Console.WriteLine($"{BucketVal}/{BucketVal - 3}, {BucketVal}/{BucketVal + 3}," +
-                        $"{BucketVal}/{BucketVal + 1}");
-                }
-                else
-                {
-                    Console.WriteLine($"{BucketVal}/{BucketVal - 3}, {BucketVal}/{BucketVal + 3}" +
-                        $"{BucketVal}/{BucketVal + 1},{BucketVal}/{BucketVal - 1}");
-                }
+                b += 3;
+                Streets();
+            }
+            else if (b > 34)
+            {
+                b -= 3;
+                Streets();
+            }
+            else
+            {
+                b += 3;
+                Streets();
+                b -= 6;
+                Streets();
             }
 
+        }
+        public static void Splits()
+        {
+            int x = b % 3;
+            if (b == 0 || b == 100) Console.WriteLine("0, 00");
+            else if (b < 3)
+            {
+                if (x == 0) Console.WriteLine($"{b}/{b + 3}, {b}/{b - 1}");
+                else if (x == 1) Console.WriteLine($"{b}/{b + 3}, {b}/{b + 1}");
+                else Console.WriteLine($"{b}/{b + 3}, {b}/{b + 1},{b}/{b - 1}");
+            }
+            else if (b < 34)
+            {
+                if (x == 0) Console.WriteLine($"{b}/{b - 3}, {b}/{b + 3}, {b}/{b - 1}");
+                else if (x == 1) Console.WriteLine($"{b}/{b - 3}, {b}/{b + 3}, {b}/{b + 1}");
+                else Console.WriteLine($"{b}/{b - 3}, {b}/{b + 3}, {b}/{b + 1},{b}/{b - 1}");
+            }
+            else
+            {
+                if (x == 0) Console.WriteLine($"{b}/{b - 3}, {b}/{b - 1}");
+                else if (x == 1) Console.WriteLine($"{b}/{b - 3}, {b}/{b + 1}");
+                else Console.WriteLine($"{b}/{b - 3}, {b}/{b + 1},{b}/{b - 1}");
+            }
+        }
+        public static void Corners()
+        {
+            string RightDown = $"{b},{b + 1},{b + 3},{b + 4}";
+            string LeftDown = $"{b },{ b - 1},{ b + 2},{ b + 3}";
+            string RightUp = $"{b},{b + 1},{b - 3},{b - 2}";
+            string LeftUp = $"{b},{b - 1},{b - 3},{b - 4}";
+            if (b == 0 || b == 100) Console.WriteLine("No corners here, bud");
+            else if (b == 1) Console.WriteLine(RightDown);
+            else if (b == 2) Console.WriteLine($"{RightDown} and {LeftDown}");
+            else if (b == 3) Console.WriteLine($"{LeftDown}");
+            else if (b == 34) Console.WriteLine($"{RightUp}");
+            else if (b == 35) Console.WriteLine($"{RightUp} and {LeftUp} ");
+            else if (b == 36) Console.WriteLine($"{RightDown}");
+            else if (b%3 == 1) Console.WriteLine($"{RightDown} and {RightUp}");
+            else if (b%3 == 2) Console.WriteLine($"{LeftDown} and {LeftUp} and {RightDown} and {RightUp}");
+            else Console.WriteLine($"{LeftDown} and {LeftUp}");
         }
     }
 }
