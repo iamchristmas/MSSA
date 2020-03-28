0f87c78f527a35b9dd8da143ce194533c81be1b6
Chris <phatman1776@gmail.com>
2020-03-28 13:14:31 -0700

    Added KeyValue Exercise

---
+++ KeyValue/KeyValueStore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyValueAssignment
+{
+    public struct KeyValue
+    {
+        public readonly string Key;
+        public readonly int Value;
+        public KeyValue(string s, int i)
+        {
+            Key = s;
+            Value = i;
+        }
+    }
+    public class MyDictionary
+    {
+        private KeyValue[] store;
+        private int count;
+        public MyDictionary()
+        {
+            store = new KeyValue[2];
+            count = 0;
+        }
+
+        public int Count { get; set; }
+        public int this[string key]
+        {
+            get => FindKeyValue(key);
+            set
+            {
+                try
+                {
+                    store[FindKeyValue(key)] = new KeyValue(key, value);
+                }
+                catch
+                {
+                    store[count] = new KeyValue(key, value);
+                    count++;
+                }
+            }
+        }
+
+        public int FindKeyValue (string s)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (store[i].Key == s) return store[i].Value;
+            }
+            throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ KeyValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Create a basic key/value store:
+
+//    Create a struct named `KeyValue` which contains one `string` "key" and one `object` "value" as `public readonly` instance fields
+//    Implement a constructor for `KeyValue` which sets the instance fields
+//    Create a class named `MyDictionary` which contains one array of KeyValue structs and one `int` to keep track of the number of stored values as private instance fields.You may choose a reasonable fixed size for the array. 
+//    Implement an indexer which takes a string (key) and returns an object (value).
+//        The `set` property should search the array for the given key and replace the KeyValue with a `new KeyValue(...)` if it exists.If the key does not exist, it should be added as a `new KeyValue(...)`.
+//        The `get` property should search the array for the given key and return the associated value if it exists.If the key does not exist, the property should throw a KeyNotFoundException.
+//    Your code should compile against the following `Main` method and print a KeyNotFoundException followed by the line "42, 17" to the command line.
+
+namespace KeyValueAssignment
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+}
