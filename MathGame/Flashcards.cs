aab0dc21da7c34957446f2f7b9e5f39e28c9d18c
Chris <phatman1776@gmail.com>
2020-04-06 01:14:38 -0700

    Added MathGame

---
+++ MathGame/Flashcards.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathGame
+{
+    class Flashcards
+    {
+        public int Score { get; set; }
+        public int NumQuestions { get; set; }
+        int minValue;
+        int maxValue;
+        Random r = new Random();
+
+        public Flashcards()
+        {
+            NumQuestions = 4;
+            minValue = 1;
+            maxValue = 12;
+        }
+        public Flashcards(int questions, int min, int max)
+        {
+            NumQuestions = questions;
+            minValue = min;
+            maxValue = max;
+        }
+
+        public int Addition()
+        {
+            int correct = NumQuestions;
+            for (int i = 1; i <= NumQuestions; i++)
+            {
+                int val1 = r.Next(minValue, maxValue);
+                int val2 = r.Next(minValue, maxValue);
+                Console.Write($"{i}. {val1} + {val2} = ");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == val1 + val2)
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, the correct answer is  {val1 + val2} :/");
+                    correct--;
+                }
+            }
+            return Score = correct * 100 / NumQuestions;
+        }
+        public int Subtraction()
+        {
+            int correct = NumQuestions;
+            for (int i = 1; i <= NumQuestions; i++)
+            {
+                int val1 = r.Next(minValue, maxValue);
+                int val2 = r.Next(minValue, maxValue);
+                if (val1 < val2)
+                {
+                    int t = val1;
+                    val1 = val2;
+                    val2 = t;
+                }
+                Console.Write($"{i}. {val1} - {val2} = ");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == val1 - val2)
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, the correct answer is  {val1 - val2} :/");
+                    correct--;
+                }
+            }
+            return Score = correct * 100 / NumQuestions;
+        }
+        public int Multiplication()
+        {
+            int correct = NumQuestions;
+            for (int i = 1; i <= NumQuestions; i++)
+            {
+                int val1 = r.Next(minValue, maxValue);
+                int val2 = r.Next(minValue, maxValue);
+                Console.Write($"{i}. {val1} X {val2} = ");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == val1 * val2)
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, the correct answer is  {val1 * val2} :/");
+                    correct--;
+                }
+            }
+            return Score = correct * 100 / NumQuestions;
+        }
+        public int Division()
+        {
+            int correct = NumQuestions;
+            for (int i = 1; i <= NumQuestions; i++)
+            {
+                float val1 = r.Next(minValue, maxValue);
+                float val2 = r.Next(minValue, maxValue);
+                Console.Write($"{i}. {val1} / {val2} = ");
+                float answer = float.Parse(Console.ReadLine());
+                if (answer + 0.1 > val1 / val2 && answer - 0.1 < val1 / val2)
+                {
+                    Console.WriteLine("Close counts!");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, the correct answer is  {val1/val2} :/");
+                    correct--;
+                }
+            }
+            return Score = correct * 100 / NumQuestions;
+        }
+
+    }
+}
---
+++ MathGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MathGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Output number of correct answers as well
+            Flashcards flashcards = new Flashcards();
+            Console.WriteLine("Welcome to Math Game, what kind of arithmetic would you like to practice?:");
+            Console.WriteLine("1. Addition ");
+            Console.WriteLine("2. Subtraction ");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("3. Division");
+            Console.WriteLine("Enter Selection ");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    flashcards.Addition();
+                    break;
+                case 2:
+                    flashcards.Subtraction();
+                    break;
+                case 3:
+                    flashcards.Multiplication();
+                    break;
+                case 4:
+                    flashcards.Division();
+                    break;
+            }
+            Console.WriteLine($"Congrats, you scored {flashcards.Score}");
+        }
+    }
+}
