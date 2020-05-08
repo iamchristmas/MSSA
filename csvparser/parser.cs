27dabd23a7b7f042e34cffa3e3c123e81f9222c6
Chris Smith <phatman1776@gmail.com>
2020-05-08 08:57:20 -0700

    Refactored parser

--- csvparser/parser.cs
+++ csvparser/parser.cs
@@ -6,22 +6,14 @@ namespace CSVParser
     {
         public static List<string> GetCSV(string s)
         {
-            List<string> output = new List<string>();
-            string temp = "";
-            for (int i = 0; i < s.Length; i++)                     
+            var output = new List<string>();
+            var temp = "";
+            for (var i = 0; i < s.Length; i++)
             {
                 if (s[i] == '\"')
-                {
                     while (s[++i] != '\"')
-                    {
                         temp += s[i];
-                    }
-                    
-                }
-                else if(s[i] != ',')
-                {
-                    temp += s[i];
-                }
+                else if (s[i] != ',') temp += s[i];
                 if (s[i] == ',')
                 {
                     output.Add(temp);
